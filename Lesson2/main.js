d3606143071d0461d4e09ba4bb8bb62693dc7c9d
matanatteyyub <teyyubovametanet@gmail.com>
2022-06-01 23:55:35 +0400

    lesson2

---
+++ Lesson2/main.js
@@ -0,0 +1,114 @@
+//OPERATOR
+//1.Riyazi
+//2.Menimsetme
+
+
+ // +
+  
+//  let x = 3;
+//  let y = 4;
+
+//  console.log(x+y);
+
+//  let a ="Hevva";
+//  let b ="Metanet";
+//  console.log(a+b);
+
+//  let x = 0.01;
+//  let y = 0.2;
+//  console.log(x+y);
+
+// -
+// *
+// /
+// %
+
+
+// let a = 12;
+// let b = 6;
+// console.log(a%b);
+
+
+//   **
+
+// let a = 3;
+// let b = 4;
+// console.log(a**b);
+
+// ++
+// --
+
+//  let a = 4;
+//  console.log(++a);
+
+//  for(let i= 0 ;i<=10 ;++i){
+//      console.log(i);
+//  }
+
+//  let a = 4;
+//  console.log(--a);
+
+
+//2.Meenimsetme
+
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// <<=	x <<= y	x = x << y
+// >>=	x >>= y	x = x >> y
+// >>>=	x >>>= y	x = x >>> y
+// &=	x &= y	x = x & y
+// ^=	x ^= y	x = x ^ y
+// |=	x |= y	x = x | y
+// **=	x **= y	x = x ** y
+
+
+// STRING
+
+// let x = "mmmmmm ysdysy \'dyy iiiooiiooi";
+// let y = "mmmmmm ysdysy \d dyy iiiooiiooi";
+// console.log(x);
+// console.log(y);
+
+// slice(start, end);
+// substring(start,end);
+// substr(start,length);
+
+// let a = "HavvaMetanet";
+// // console.log(a.slice(1,4));
+// // console.log(a.substring(1,4));
+// console.log(a.substr(2,5));
+
+
+// NUMBER
+///stringleri numbere cevirmek
+//1.Number();  
+// 2.parseInt();
+// 2.parseFloat()
+
+// let x= "2";
+// console.log(typeof(Number(x)));
+
+
+
+
+//String
+
+//toString
+
+let x = 3;
+console.log(typeof(x.toString()));
+
+
+
+
+
+
+
+//  \'
+//  \"
+//  \\
+
