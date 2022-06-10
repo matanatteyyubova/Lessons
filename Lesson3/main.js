39ab2f4b00aaa2e0ae80e28f1b7ceb9629afb3db
matanatteyyub <teyyubovametanet@gmail.com>
2022-06-10 23:45:31 +0400

    string methods

---
+++ Lesson3/main.js
@@ -0,0 +1,137 @@
+// ==  //value lari muqayise edir
+// ===
+// !=
+// >
+// <
+// <=
+// >=
+
+// console.log(4==4); 
+// console.log(4=="4");
+// console.log(4==="4");
+
+
+// console.log(4 != 4);
+
+// console.log(4 <= 47);
+
+
+// Şərt Operatorları
+
+//if else  
+//Switch case
+
+// if(wert){
+   
+// }
+// else {
+
+// }
+
+// var x = 5;
+//  if (x== 5)console.log("Cavab 5'dir");
+
+//  else console.log("wert yanlisdir");
+ 
+
+// var x= 6;
+//  if ( x == 7){
+//      console.log("Cavab 6 dir");
+//  }
+//   else if ( x== 4){
+//     console.log("Cavab 4 dir");
+// }
+// else {
+//     console.log("Shertler dogru deyil");
+// }
+
+// Ternary
+
+// var x=5;
+// // shert? "dogru olduqda":"sehv olduqda"
+// console.log( x==5 ? "cavab dogrudur" :"Cavab sehvdir");
+
+
+//Switch Case
+//  var x = 5
+// switch (x) {
+//    case 6:
+//        console.log("havva");
+//       break;
+//    case 4:
+//       console.log("Matanat");
+//       break;
+//    case 7:
+//       console.log("Mat");
+//       break;
+
+//    default:
+//     console.log("Dogru cavab yoxdur");
+//       break;
+// }
+
+
+
+
+
+
+
+// Function teyin etmek
+
+//1.
+// function MyFunc(){
+//    console.log("hello");
+// }
+//  MyFunc();
+
+
+
+// //2.
+// MyFunc = function() {
+// console.log("salam");
+// }
+// MyFunc()
+
+
+
+// // 3.
+// MyFunc =()=>{
+//    console.log("Havva");
+// }
+
+//  MyFunc()
+
+// Oz -ozunu cagiran funksiyalar
+
+// (myFunc =() =>{
+//    console.log("Tapwiriqlarini vaxtinda et!!!");
+// })()
+
+
+
+//Deyisen metodlari
+   //.search()
+//  var m = "HaMvaMatanat";
+//  var y = m.search("M");
+//  console.log(y);
+
+  
+  //concat()
+
+//   var m = "Havva";
+//   var n =  "Matanat"
+//   var y = n.concat(m)
+//      console.log(y);
+
+//indexOf
+ 
+// var n = "Havva"
+// var y = n.indexOf("H")
+// console.log(y);
+
+
+//split()
+
+// var n = "Derslerinle/ciddi mesgul ol!!!"
+// var y = n.split("/");
+// console.log(y);
