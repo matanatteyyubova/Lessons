3d6d2e13054d7ff3bb2d60540da92319f620177b
matanatteyyub <teyyubovametanet@gmail.com>
2022-06-19 23:20:31 +0400

    loop

---
+++ Lesson4/main.js
@@ -0,0 +1,51 @@
+//Dövrlər
+
+//For
+//For in
+//For of
+//While
+// do while
+
+//WHILE*************************
+
+// while(shert){
+//     //icra olonacaq emeliyyat
+// }
+
+
+// let i = 2;
+// while(i< 1){
+//     console.log(i);
+//     i++;
+// }
+
+
+ 
+// do
+   //icra olonacaq emeliyyat
+
+//while(shert)
+
+// let i= 2;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i <1)
+
+
+//FOR
+
+// for(let i = 0;i<10; i++){
+//     console.log(i);    
+// }
+
+
+//For in
+
+// let cars ={ name:"BMW",speed:"500"}
+// console.log(cars);
+
+// for (x in cars){
+//     console.log(cars[x]);
+// }
